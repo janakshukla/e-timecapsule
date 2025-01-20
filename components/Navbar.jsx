d8a897211575c95ab431d77708cf10c11a39dfc1
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-01-20 10:31:28 +0530

    Add mobile navigation with hamburger menu to Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,11 @@
+"use client"
 import React from "react";
 import Link from "next/link";
+
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
     <nav className="from-transparent bg-gradient-to-b to-primary-100 p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center text-white">
@@ -27,6 +30,47 @@ const Navbar = () => {
               </Link>
             </div>
             <UserButton />
+            {/* Links with Hamburger for mobile users */}
+            <div className="sm:hidden">
+              <button
+                className="text-white focus:outline-none"
+                aria-label="Open Menu"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <svg
+                  className="w-6 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              </button>
+              <div>
+                {isOpen && (
+                  <div className="  text-white p-4">
+                    <Link href="/" onClick={() => setIsOpen(false)} className="block py-2 hover:text-blue-400">
+                      Home
+                    </Link>
+                    <Link href="/dashboard" onClick={() => setIsOpen(false)} className="block py-2 hover:text-blue-400">
+                      Dashboard
+                    </Link>
+                    <Link href="/alluser" onClick={() => setIsOpen(false)} className="block py-2 hover:text-blue-400">
+                      All Users
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </div>
+
+
+
           </SignedIn>
 
           <SignedOut>
