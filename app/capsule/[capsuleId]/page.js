4c89b62874c20e5e81ae70575a8ecaacae4fa37f
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-01-31 10:46:32 +0530

    Enhance capsule detail page layout with improved styling and additional information

--- app/capsule/[capsuleId]/page.js
+++ app/capsule/[capsuleId]/page.js
@@ -18,13 +18,17 @@ export default async function capsules({ params }) {
   }
 
   return (
-    <Suspense fallback={<Loader />}>
-      <div className="flex flex-col items-center p-6 bg-gray-100 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold mb-4 text-gray-800">{capsule.title}</h1>
-        <p className="text-lg mb-4 text-gray-600">{capsule.description}</p>
-        <p className="text-md mb-6 text-gray-500">{JSON.stringify(capsule.opendate)}</p>
+   <div className="flex justify-center items-center bg-primary-300 h-screen">
+     <Suspense fallback={<Loader />}>
+     <div className="flex flex-col items-center justify-center h-3/2  bg-white p-8 rounded-lg shadow-lg">
         <img className="rounded-lg shadow-md" src={capsule.image} width={200} height={200} />
-      </div>
+      
+        <h1 className="text-3xl font-bold  mb-4 text-primary-300">{capsule.title}</h1>
+        <p className="text-lg mb-4 text-gray-600">{capsule.description}</p>
+        <p className="text-md mb-6 text-gray-500"><span className="text-primary-300 font-semibold" >OPEN DATE:</span> {JSON.stringify(Date(capsule.opendate))}</p>
+        <p className="text-md mb-6 text-gray-500"><span className="text-primary-300 font-semibold" >CreatedAT:</span> {JSON.stringify(Date(capsule.createdAt))}</p>
+        </div>
     </Suspense>
+   </div>
   );
 }
