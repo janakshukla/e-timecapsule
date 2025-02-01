4b1cc0f7b4e304f3791b32d39a4a601647be5f16
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-02-01 10:34:09 +0530

    Refactor capsule detail page layout and enhance navbar for improved user experience

--- app/capsule/[capsuleId]/page.js
+++ app/capsule/[capsuleId]/page.js
@@ -1,5 +1,7 @@
 import Loader from "@/components/Loader";
 import { prisma } from "@/prisma/index";
+import { redirect } from "next/navigation";
+
 
 import { Suspense } from "react";
 
@@ -18,17 +20,49 @@ export default async function capsules({ params }) {
   }
 
   return (
-   <div className="flex justify-center items-center bg-primary-300 h-screen">
-     <Suspense fallback={<Loader />}>
-     <div className="flex flex-col items-center justify-center h-3/2  bg-white p-8 rounded-lg shadow-lg">
-        <img className="rounded-lg shadow-md" src={capsule.image} width={200} height={200} />
-      
-        <h1 className="text-3xl font-bold  mb-4 text-primary-300">{capsule.title}</h1>
-        <p className="text-lg mb-4 text-gray-600">{capsule.description}</p>
-        <p className="text-md mb-6 text-gray-500"><span className="text-primary-300 font-semibold" >OPEN DATE:</span> {JSON.stringify(Date(capsule.opendate))}</p>
-        <p className="text-md mb-6 text-gray-500"><span className="text-primary-300 font-semibold" >CreatedAT:</span> {JSON.stringify(Date(capsule.createdAt))}</p>
+
+    <div className="flex justify-center items-center bg-primary-300 h-screen">
+      <Suspense fallback={<Loader />}>
+        <div className="flex flex-col items-center justify-center  backdrop-blur-lg shadow-slate-800   bg-primary-300/50 p-8 rounded-lg shadow-lg">
+          <div className="flex items-center justify-between w-full mb-4">
+            <h1 className="text-3xl font-bold mb-4 text-secondry-300">{capsule.title}</h1>
+            <form
+              action={async () => {
+                "use server";
+                await prisma.capsule.delete({
+                  where: {
+                    id: capsule.id,
+                  },
+                });
+                redirect("/dashboard");
+              }}
+
+            >
+              <button className="text-red-500 hover:text-red-700">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </form>
+          </div>
+          <img className="rounded-lg shadow-md" src={capsule.image} width={200} height={200} />
+
+          <p className="text-lg mb-4 text-gray-600">{capsule.description}</p>
+          <p className="text-md mb-6 text-gray-500"><span className="text-secondry-300 font-semibold" >OPEN DATE:</span> {JSON.stringify(Date(capsule.opendate))}</p>
+          <p className="text-md mb-6 text-gray-500"><span className="text-secondry-300 font-semibold" >CreatedAT:</span> {JSON.stringify(Date(capsule.createdAt))}</p>
         </div>
-    </Suspense>
-   </div>
+      </Suspense>
+    </div>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Link from "next/link";
 
@@ -7,96 +7,110 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState(false);
   return (
-    <nav className="from-transparent bg-gradient-to-b to-primary-100 p-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center text-white">
-        {/* Brand Name */}
-        <Link href="/" className="md:text-3xl font-extrabold font-mono hover:text-blue-400">
-          E-TimeCapsule
-        </Link>
+    <>
+      <nav className="from-transparent bg-gradient-to-b to-primary-100 p-4 shadow-md">
+        <div className="container mx-auto flex justify-between items-center text-white">
+          {/* Brand Name */}
+          <Link
+            href="/"
+            className="md:text-3xl font-extrabold font-mono hover:text-blue-400"
+          >
+            E-TimeCapsule
+          </Link>
 
-        {/* Navigation Links */}
-        <div className="flex items-center space-x-6">
-          <SignedIn>
-            {/* Links for Signed-In Users */}
-            <div className="hidden sm:flex space-x-4">
-              <Link href="/" className="hover:text-blue-400">
-                Home
-              </Link>
-              <Link href="/dashboard" className="hover:text-blue-400">
-                Dashboard
-              </Link>
-              <Link href="/alluser" className="hover:text-blue-400">
-                All Users
-              </Link>
-            </div>
-            <UserButton />
-            {/* Links with Hamburger for mobile users */}
-            <div className="sm:hidden">
-              <button
-                className="text-white focus:outline-none"
-                aria-label="Open Menu"
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                <svg
-                  className="w-6 h-6"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16m-7 6h7"
-                  />
-                </svg>
-              </button>
-              <div>
-                {isOpen && (
-                  <div className="  text-white p-4">
-                    <Link href="/" onClick={() => setIsOpen(false)} className="block py-2 hover:text-blue-400">
-                      Home
-                    </Link>
-                    <Link href="/dashboard" onClick={() => setIsOpen(false)} className="block py-2 hover:text-blue-400">
-                      Dashboard
-                    </Link>
-                    <Link href="/alluser" onClick={() => setIsOpen(false)} className="block py-2 hover:text-blue-400">
-                      All Users
-                    </Link>
-                  </div>
-                )}
+          {/* Navigation Links */}
+          <div className="flex items-center space-x-6">
+            <SignedIn>
+              {/* Links for Signed-In Users */}
+              <div className="hidden sm:flex space-x-4">
+                <Link href="/" className="hover:text-blue-400">
+                  Home
+                </Link>
+                <Link href="/dashboard" className="hover:text-blue-400">
+                  Dashboard
+                </Link>
+                <Link href="/alluser" className="hover:text-blue-400">
+                  All Users
+                </Link>
               </div>
-            </div>
-
-
-
-          </SignedIn>
-
-          <SignedOut>
-            {/* Buttons for Signed-Out Users */}
-            <div className="flex space-x-4">
-              <Link href="/sign-up">
+              <UserButton />
+              {/* Links with Hamburger for mobile users */}
+              <div className="sm:hidden">
                 <button
-                  className="bg-primary-300 hover:bg-blue-600 text-secondry-100 text-xs md:text-base py-2 px-4 rounded transition duration-300"
-                  aria-label="Sign Up"
+                  className="text-white focus:outline-none"
+                  aria-label="Open Menu"
+                  onClick={() => setIsOpen(!isOpen)}
                 >
-                  Sign Up
+                  <svg
+                    className="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
                 </button>
-              </Link>
-              <Link href="/sign-in">
-                <button
-                  className=" bg-primary-300  hover:bg-blue-600 text-xs text-secondry-100 md:text-base py-2 px-4 rounded transition duration-300"
-                  aria-label="Log In"
-                >
-                  Log In
-                </button>
-              </Link>
-            </div>
-          </SignedOut>
+              </div>
+            </SignedIn>
+
+            <SignedOut>
+              {/* Buttons for Signed-Out Users */}
+              <div className="flex space-x-4">
+                <Link href="/sign-up">
+                  <button
+                    className="bg-primary-300 hover:bg-blue-600 text-secondry-100 text-xs md:text-base py-2 px-4 rounded transition duration-300"
+                    aria-label="Sign Up"
+                  >
+                    Sign Up
+                  </button>
+                </Link>
+                <Link href="/sign-in">
+                  <button
+                    className=" bg-primary-300  hover:bg-blue-600 text-xs text-secondry-100 md:text-base py-2 px-4 rounded transition duration-300"
+                    aria-label="Log In"
+                  >
+                    Log In
+                  </button>
+                </Link>
+              </div>
+            </SignedOut>
+          </div>
         </div>
+      </nav>
+      <div>
+        {isOpen && (
+          <div className="  text-white p-4">
+            <Link
+              href="/"
+              onClick={() => setIsOpen(false)}
+              className="block py-2 hover:text-blue-400"
+            >
+              Home
+            </Link>
+            <Link
+              href="/dashboard"
+              onClick={() => setIsOpen(false)}
+              className="block py-2 hover:text-blue-400"
+            >
+              Dashboard
+            </Link>
+            <Link
+              href="/alluser"
+              onClick={() => setIsOpen(false)}
+              className="block py-2 hover:text-blue-400"
+            >
+              All Users
+            </Link>
+          </div>
+        )}
       </div>
-    </nav>
+    </>
   );
 };
 
