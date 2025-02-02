88b59b77bbf43af16c412cb05fa93bd96a88095c
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-02-02 15:10:05 +0530

    Enhance global error page styling and add scrollbar customization; update home page with welcome message and navigation button

--- app/global-error.js
+++ app/global-error.js
@@ -1,12 +1,16 @@
-'use client' 
- 
+'use client'
+
 export default function GlobalError({ error, reset }) {
   return (
-    
-    <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+    <html className="h-full bg-gray-100">
+      <body className="flex flex-col items-center justify-center min-h-screen p-4">
+        <h2 className="text-2xl font-bold mb-4 text-red-600">Something went wrong!</h2>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+          onClick={() => reset()}
+        >
+          Try again
+        </button>
       </body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 
@@ -5,9 +6,18 @@ export default function Home() {
 
 
   return (
-    <div className=""  >
-
-      
+    <div className="min-h-screen flex flex-col items-center justify-center bg-primary-300
+    text-secondry-100 py-10">
+      <section className="text-center space-y-4">
+        <h1 className="text-4xl font-bold">Welcome to E-Time Capsule</h1>
+        <p className="text-lg">
+          Capture and preserve your special moments for the future.
+        </p>
+        <Link href="/dashboard"> <button  className="px-6 mt-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-500">
+          Get Started
+        </button>
+        </Link>
+      </section>
     </div>
   );
 }
