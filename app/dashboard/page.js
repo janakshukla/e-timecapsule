3d17ea928b891378c9125f07c3dc5320f39fa70e
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-02-08 09:26:40 +0530

    Add loading state and no capsules message to dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -6,12 +6,13 @@ import { useEffect, useState } from "react";
 
 export default function Dashboard() {
   const [capsules, setcapsules] = useState([])
+  const[ isloaded, setisloaded] = useState(false)
 
   useEffect(() => {
     const getcapsules = async () => {
       const response = await fetch('/api/capsules').then((res) => res.json()).catch((error) => console.error(error));
       setcapsules(response);
-
+      setisloaded(true)
 
     }
     getcapsules()
@@ -31,7 +32,12 @@ export default function Dashboard() {
       </Link>
 
       <div className="mt-4 grid grid-cols-1  object-center md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {capsules?.length > 0 ? (
+        { isloaded ? (
+          capsules.length === 0 ? (
+            <div className="w-full col-span-3 flex justify-center items-center">
+              <p className="text-gray-400">No Capsules Found</p>
+            </div>
+          ) : ( 
           capsules.map((capsule) => (
             <Link href={`/capsule/${capsule.id}`} 
               key={capsule.id}
@@ -57,7 +63,7 @@ export default function Dashboard() {
                 <span className="font-bold text-gray-200">updatedAt:</span>{" "}
                 {new Date(capsule.updatedAt).toLocaleDateString()}
               </p>
-            </Link>
+            </Link>)
           ))
         ) : (
           <div className="w-full col-span-3 flex justify-center items-center ">
