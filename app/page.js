d3201ca5134a2d6efb2021f8af42d98262e6dc97
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-07-09 21:01:32 +0530

    Add Hero and Feature components to enhance Home page layout and user experience

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import { StarHalf, StarIcon } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+import Feature from "@/components/Feature";
+import Hero from "@/components/Hero";
+
 
 
 
@@ -8,61 +8,9 @@ export default function Home() {
 
 
   return (
-    <div className="flex max-sm:flex-col items-center lg:gap-36 h-[80dvh] lg:px-36 ">
-
-      {/* left div */}
-      <div
-        className="sm:w-1/2 h-full w-svw max-sm:h-1/2 text-white "
-      >
-        {/* upper section */}
-        <section>
-          <h1
-            className="text-3xl m-auto text-center tracking-tighter sm:text-5xl mt-16 leading-tight md:mt-32 font-bold "
-          >Preserve your memories for the future.</h1>
-          <p
-            className="text-gray-300 mx-8 lg:mx-32 text-xs md:text-sm text-center text-wrap leading-normal"
-          >Whether it's a future surprise, a goal reminder, or a message to your future self — we’ll make sure it arrives on time.</p>
-
-        </section>
-        {/* lower section */}
-        <section className="sm:mt-24" >
-          <div className="flex flex-row w-full">
-            <div className="text-center w-1/2"  >
-              <h1 className="text-2xl  sm:text-4xl font-bold"  >100%</h1>
-              <p className="text-xs sm:text-sm text-gray-300 " >secure and reliable.</p>
-            </div>
-            <div className=" text-center w-1/2"  >
-              <h1 className="text-2xl  sm:text-4xl font-bold"  >20+</h1>
-              <p className="text-xs sm:text-sm text-gray-300 " >grabbed user attention.</p>
-            </div>
-          </div>
-          <div>
-            <h1 className="flex font-bold  size-24 sm:44 justify-center w-full gap-1 mt-8" >
-              <StarIcon fill="#FFFFFF" />
-              <StarIcon fill="#FFFFFF" />
-              <StarIcon fill="#FFFFFF" />
-              <StarIcon fill="#FFFFFF" />
-              <StarHalf fill="#FFFFFF" />
-              <span className="mt-1" >4.5</span>
-              <span className="text-xs mt-2 text-gray-400 " >Average user rating.</span>
-            </h1>
-          </div>
-
-        </section>
-
-      </div>
-      {/*Right div*/}
-      <div
-        className="sm:w-1/2 h-full mt-32 sm:mt-52 w-svw max-sm:h-1/2 "
-      >
-        <Image
-          src="/bgimage.webp" 
-          alt="A descriptive alt text" 
-           width={440}
-           height={440}
-        />
-      </div>
-
+    <div>
+      <Hero />
+      <Feature />
     </div>
   );
 }
---
+++ components/Feature.jsx
@@ -0,0 +1,78 @@
+import { Archive, Calendar, LockIcon, Mail, Share, Timer, VideoIcon } from "lucide-react";
+import React from "react";
+
+const Feature = () => {
+  return (
+    <div className="flex max-sm:mt-56 items-center lg:gap-36 h-[80dvh] md:pt-16 lg:px-36">
+      <div className="md:grid grid-cols-6 h-full w-full text-white grid-rows-7 gap-4">
+        <div className="col-span-4 row-span-2 p-4 border rounded-md shadow-lg ">
+          <Timer className=" size-10  rounded-md bg-red-300 text-red-800 p-2 " />
+          <h1 className="text-xl font-bold tracking-tight leading-snug">
+            Time-Locked Delivery
+          </h1>
+          <p className=" font-light text-xs lg:text-base  tracking-wide leading-tight">
+            Schedule messages, images, or videos to unlock on a future date.
+          </p>
+        </div>
+        <div className="col-span-2 row-span-2 col-start-5 p-4 border rounded-md shadow-lg ">
+          {" "}
+          <Mail className=" size-10  rounded-md bg-blue-300 text-blue-800 p-2 " />
+          <h1 className="text-xl font-bold tracking-tight leading-snug">
+          Email Notifications
+          </h1>
+          <p className=" font-light text-xs lg:text-base tracking-wide leading-tight">
+          Automatically notify recipients when a capsule opens.
+          </p>
+        </div>
+        <div className="col-span-2 row-span-3 row-start-3 p-4 border rounded-md shadow-lg ">
+          {" "}
+          <LockIcon className=" size-10  rounded-md bg-purple-300 text-purple-800 p-2 " />
+          <h1 className="text-xl font-bold tracking-tight leading-snug">
+          End-to-End Privacy
+          </h1>
+          <p className=" font-light text-xs lg:text-base tracking-wide leading-tight">
+          Your memories are encrypted and secure — only you control the unlock time.
+          </p>
+        </div>
+        <div className="col-span-2 row-span-2 col-start-3 p-4 border rounded-md shadow-lg row-start-3  ">
+          <VideoIcon className=" size-10  rounded-md bg-violet-300 text-violet-800 p-2 " />
+          <h1 className="text-xl font-bold tracking-tight leading-snug">
+          Multimedia Support
+          </h1>
+          <p className=" font-light text-xs lg:text-base tracking-wide leading-tight">
+          Attach text, images, videos, or files to your capsules.
+          </p>
+        </div>
+        <div className="col-span-2 row-span-3 col-start-3 row-start-5 p-4 border rounded-md shadow-lg ">
+          <Calendar className=" size-10  rounded-md bg-green-300 text-green-800 p-2 " />
+          <h1 className="text-xl font-bold tracking-tight leading-snug">
+          Smart Scheduler
+          </h1>
+          <p className=" font-light text-xs lg:text-base tracking-wide leading-tight">
+          Choose dates like birthdays, anniversaries, or custom timelines.
+          </p>
+        </div>
+        <div className="col-span-2 row-span-2 col-start-1 row-start-6 p-4 border rounded-md shadow-lg  ">
+          <Share className=" size-10  rounded-md bg-orange-300 text-orange-800 p-2 " />
+          <h1 className="text-xl font-bold tracking-tight leading-snug">
+          Share with Loved Ones
+          </h1>
+          <p className=" font-light text-xs lg:text-base tracking-wide leading-tight">
+          Send a capsule to friends, family, or even your future self.
+          </p>
+        </div>
+        <div className="col-span-2 row-span-5 col-start-5 row-start-3 p-4 border rounded-md shadow-lg  ">
+          <Archive className=" size-10  rounded-md bg-yellow-300 text-yellow-800 p-2 " />
+          <h1 className="text-xl font-bold tracking-tight leading-snug">
+          Capsule Preview
+          </h1>
+          <p className=" font-light text-xs lg:text-base tracking-wide leading-tight">
+          See how your capsule will appear when it opens (only if allowed).
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feature;
