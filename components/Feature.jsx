1b6a7db5c51c54abc61f6deaca27f3d880354fd7
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-07-09 21:25:17 +0530

    Refactor Feature component to remove unnecessary border styles and improve layout consistency

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -5,7 +5,7 @@ const Feature = () => {
   return (
     <div className="flex mt-16 items-center lg:gap-36 min-h-[80dvh] md:pt-16 lg:px-36">
       <div className="md:grid grid-cols-6 h-full w-full text-white grid-rows-7 gap-4">
-        <div className="col-span-4  row-span-2 p-4 border rounded-md shadow-lg ">
+        <div className="col-span-4 row-span-2 p-4  rounded-md shadow-lg ">
           <Timer className=" size-10  rounded-md bg-red-300 text-red-800 p-2 " />
           <h1 className="text-xl font-bold tracking-tight leading-snug">
             Time-Locked Delivery
@@ -14,7 +14,7 @@ const Feature = () => {
             Schedule messages, images, or videos to unlock on a future date.
           </p>
         </div>
-        <div className="col-span-2 row-span-2 col-start-5 p-4 border rounded-md shadow-lg ">
+        <div className="col-span-2 row-span-2 col-start-5 p-4  rounded-md shadow-lg ">
           {" "}
           <Mail className=" size-10  rounded-md bg-blue-300 text-blue-800 p-2 " />
           <h1 className="text-xl font-bold tracking-tight leading-snug">
@@ -24,7 +24,7 @@ const Feature = () => {
           Automatically notify recipients when a capsule opens.
           </p>
         </div>
-        <div className="col-span-2 row-span-3 row-start-3 p-4 border rounded-md shadow-lg ">
+        <div className="col-span-2 row-span-3 row-start-3 p-4  rounded-md shadow-lg ">
           {" "}
           <LockIcon className=" size-10  rounded-md bg-purple-300 text-purple-800 p-2 " />
           <h1 className="text-xl font-bold tracking-tight leading-snug">
@@ -34,7 +34,7 @@ const Feature = () => {
           Your memories are encrypted and secure — only you control the unlock time.
           </p>
         </div>
-        <div className="col-span-2 row-span-2 col-start-3 p-4 border rounded-md shadow-lg row-start-3  ">
+        <div className="col-span-2 row-span-2 col-start-3 p-4  rounded-md shadow-lg row-start-3  ">
           <VideoIcon className=" size-10  rounded-md bg-violet-300 text-violet-800 p-2 " />
           <h1 className="text-xl font-bold tracking-tight leading-snug">
           Multimedia Support
@@ -43,7 +43,7 @@ const Feature = () => {
           Attach text, images, videos, or files to your capsules.
           </p>
         </div>
-        <div className="col-span-2 row-span-3 col-start-3 row-start-5 p-4 border rounded-md shadow-lg ">
+        <div className="col-span-2 row-span-3 col-start-3 row-start-5 p-4  rounded-md shadow-lg ">
           <Calendar className=" size-10  rounded-md bg-green-300 text-green-800 p-2 " />
           <h1 className="text-xl font-bold tracking-tight leading-snug">
           Smart Scheduler
@@ -52,7 +52,7 @@ const Feature = () => {
           Choose dates like birthdays, anniversaries, or custom timelines.
           </p>
         </div>
-        <div className="col-span-2 row-span-2 col-start-1 row-start-6 p-4 border rounded-md shadow-lg  ">
+        <div className="col-span-2 row-span-2 col-start-1 row-start-6 p-4  rounded-md shadow-lg  ">
           <Share className=" size-10  rounded-md bg-orange-300 text-orange-800 p-2 " />
           <h1 className="text-xl font-bold tracking-tight leading-snug">
           Share with Loved Ones
@@ -61,7 +61,7 @@ const Feature = () => {
           Send a capsule to friends, family, or even your future self.
           </p>
         </div>
-        <div className="col-span-2 row-span-5 col-start-5 row-start-3 p-4 border rounded-md shadow-lg  ">
+        <div className="col-span-2 row-span-5 col-start-5 row-start-3 p-4  rounded-md shadow-lg  ">
           <Archive className=" size-10  rounded-md bg-yellow-300 text-yellow-800 p-2 " />
           <h1 className="text-xl font-bold tracking-tight leading-snug">
           Capsule Preview
